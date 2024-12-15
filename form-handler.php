0d73af7b2f737d62827ecfb036d760518600c282
unknown <samueljosesph2k19@gmail.com>
2024-12-15 20:36:32 +0530

    first commit

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor-email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['Message'];
+
+$email_from = 'vishnuhari2k22@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n";
+
+$to = 'vishnuhari2k22@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers)
+
+header("Location: contact.html")
+
+
+?>
